8e7264f040951fe4829680da19597ff391da8129
muhamedashraf10 <muhamedashref02@gmail.com>
2022-10-06 19:53:32 +0200

    uninstall library

--- src/components/CardsOfPostsUser/CardsOfPostsUser.js
+++ src/components/CardsOfPostsUser/CardsOfPostsUser.js
@@ -1,5 +1,3 @@
-// ------------------------------------ Another Code Whithout Use library , by used useHooks (useState) -----------------------------
-
 import React, { useState } from "react";
 
 // css module
@@ -51,61 +49,3 @@ const CardsOfPostsUser = ({ posts, user }) => {
 };
 
 export default CardsOfPostsUser;
-
-// ------------ using library , react collapsed  , if use it , you should see file css , you will see comment------------------------
-
-// import React from "react";
-
-// // react collapsed -> https://www.npmjs.com/package/react-collapsed
-// import useCollapse from "react-collapsed";
-
-// // css module
-// import Styles from "./CardsOfPostsUser.module.css";
-
-// // icon
-// import { BsThreeDots, BsArrowUp, BsArrowDown } from "react-icons/bs";
-
-// const CardsOfPostsUser = ({ posts, user }) => {
-//   const { getCollapseProps, getToggleProps, isExpanded } = useCollapse({
-//     duration: 800,
-//     collapsedHeight: 1,
-//     easing: "cubic-bezier(.12,.16,.18,.12)",
-//     expandStyles: {
-//       opacity: 0.4,
-//     },
-//     collapseStyles: {
-//       opacity: 0.4,
-//     },
-//   });
-
-//   return (
-//     <article className={Styles.posts}>
-//       <div className={Styles.post} key={posts.id}>
-//         <div className={Styles.icon_Menu}>
-//           <BsThreeDots />
-//         </div>
-//         <div className={Styles.card_Content}>
-//           <img src={`../images/${user.id}.png`} alt="User Profile" />
-//           <h2>{user.username}</h2>
-//           <h3>{posts.title}</h3>
-//           <div {...getCollapseProps()} className={Styles.more_Card_Body}>
-//             <h4>{posts.body}</h4>
-//             <h5>{user.name}</h5>
-//             <h6>{user.email}</h6>
-//           </div>
-
-//           <button className={Styles.bttn} {...getToggleProps()}>
-//             {isExpanded ? "Read Less" : "Read More"}
-//             {isExpanded ? (
-//               <BsArrowUp className={Styles.icon_Arrow} />
-//             ) : (
-//               <BsArrowDown className={Styles.icon_Arrow} />
-//             )}
-//           </button>
-//         </div>
-//       </div>
-//     </article>
-//   );
-// };
-
-// export default CardsOfPostsUser;
